fecc8b0fef2fd2706937831ec01d5feeefe3b323
Maxwell Henderson <mxwhenderson@gmail.com>
2023-12-21 17:41:43 -0800

    Move creation of completion into POST request
    This is so that when you build the code you don't need an API key
    
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- src/routes/api/completion/+server.ts
+++ src/routes/api/completion/+server.ts
@@ -1,13 +1,13 @@
 import { ChatbotCompletion } from '$lib';
 
-const completion_manager = new ChatbotCompletion(import.meta.env.VITE_OPENAI_API_KEY!, {
-	openai_model: import.meta.env.VITE_OPENAI_MODEL_NAME!
-});
-
 //TODO(max): Change this function to another protocol, nothing is being POSTed
 /** @type {import('./$types').RequestHandler} */
 export async function POST(params: { request: Request }) {
 	const { chatHistory } = await params.request.json();
+
+	const completion_manager = new ChatbotCompletion(import.meta.env.VITE_OPENAI_API_KEY!, {
+		openai_model: import.meta.env.VITE_OPENAI_MODEL_NAME!
+	});
 
 	const stream = await completion_manager.query(chatHistory);
 
