c315a3500a9c485dcc0541a380ab73b7d9a36c04
charliehuang09 <charliehuang09@gmail.com>
2024-01-04 10:31:50 -0800

    edit prompt
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -1,7 +1,7 @@
-const PREFIX = `Answer the following questions as best you can. You have access to the following tools:
+const PREFIX = `You are a friendly chatbot assistent who both answers questions and talks with the user. You have access to the following tools:
 `;
 const HISTORY = `Here is the chat history: \n`;
-const TOOL_INSTRUCTIONS_TEMPLATE = `Use the following format in your response:
+const TOOL_INSTRUCTIONS_TEMPLATE = `Use the following format in your response. If you have the final answer, just say the final without using the format:
 Question: the input question you must answer
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
