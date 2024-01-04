e20edf69be075740ea10d9dced342b21c5744331
charliehuang09 <charliehuang09@gmail.com>
2024-01-03 19:17:29 -0800

    remove example code from tools.ts dynamicstructuredtool
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -32,16 +32,6 @@ function getTools() {
 			hl: 'en',
 			gl: 'us'
 		}),
-		new DynamicStructuredTool({
-			//for testing remove when merge
-			name: 'random-number-generator',
-			description: 'generates a random number between two input numbers',
-			schema: z.object({
-				low: z.number().describe('The lower bound of the generated number'),
-				high: z.number().describe('The upper bound of the generated number')
-			}),
-			func: async ({ low, high }) => (Math.random() * (high - low) + low).toString() // Outputs still must be strings
-		}),
 		new Calculator()
 	];
 	return tools;
