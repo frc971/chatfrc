9a059a7fac9b509a6809ae6253ef614cf56e3ae8
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-05 13:18:39 -0800

    Fix lint in app.d.ts
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-    declare interface Window {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        dataLayer: any;
-    }
-    namespace App {
-        // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
-        // interface Platform {}
-    }
+	declare interface Window {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		dataLayer: any;
+	}
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
 }
 
-export { };
+export {};
