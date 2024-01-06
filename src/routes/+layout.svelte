a0bfd00bb4e3e6b8bb9b973283255b8cf12a8c55
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-05 18:03:02 -0800

    Small UI tweaks
    This commit makes some changes to our navbar:
      * Makes links open in new tabs
      * Resizes 971 logo
    
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,18 @@
 	class="absolute top-0 p-2 w-full shadow-lg bg-gray-100 flex flex-1 flex-row space-x-4 justify-between pr-4 pl-4"
 >
 	<div class="inline-flex space-x-4">
-		<enhanced:img alt="FRC971 Spartan Robotics Logo" src="/static/logo.png" class="w-8 h-8" />
+		<enhanced:img alt="FRC971 Spartan Robotics Logo" src="/static/logo.png" class="w-7 h-7" />
 		<h1 class="font-bold text-xl">ChatFRC</h1>
 	</div>
 	<ul class="inline-flex space-x-4">
 		<li class="text-gray-600 text-lg hover:text-blue-600">
-			<a href="https://www.frc971.org/">Home</a>
+			<a href="https://www.frc971.org/" target="_blank">Home</a>
 		</li>
 		<li class="text-gray-600 text-lg hover:text-blue-600">
-			<a href="/">Chatbot</a>
+			<a href="/" target="_blank">Chatbot</a>
 		</li>
 		<li class="text-gray-600 text-lg hover:text-blue-600">
-			<a href="/about">About</a>
+			<a href="/about" target="_blank">About</a>
 		</li>
 	</ul>
 </nav>
