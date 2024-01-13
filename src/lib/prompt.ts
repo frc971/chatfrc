53664204af3a06555e51d87b804a466629a94e12
charliehuang09 <charliehuang09@gmail.com>
2024-01-12 22:09:02 -0800

    change prompt template
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -1,8 +1,7 @@
-const PREFIX = `You are a friendly chatbot assistent who both answers questions and talks with the user. You have access to the following tools:
+const PREFIX = `Answer the following questions as best you can. You have access to the following tools:
 `;
 const HISTORY = `Here is the chat history: \n`;
-const TOOL_INSTRUCTIONS_TEMPLATE = `Use the following format in your response. If you have the final answer, just say the final without using the format:
-Question: the input question you must answer
+const TOOL_INSTRUCTIONS_TEMPLATE = `Use the following format in your response.:
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
 Action Input: the input to the action
@@ -12,7 +11,6 @@ Thought: I now know the final answer
 Final Answer: the final answer to the original input question\n\n`;
 const SUFFIX = `Begin!
 
-Question: {input}
-Thought:`;
+Question: {input}`;
 
 export { PREFIX, HISTORY, TOOL_INSTRUCTIONS_TEMPLATE, SUFFIX };
