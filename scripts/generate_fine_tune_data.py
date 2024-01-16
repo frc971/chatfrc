c79ce36304138c5bc2a6eb9b7312b157625246ca
charliehuang09 <charliehuang09@gmail.com>
2024-01-15 16:40:55 -0800

    add train test split for generate_fine_tune_data
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- scripts/generate_fine_tune_data.py
+++ scripts/generate_fine_tune_data.py
@@ -24,7 +24,16 @@ for i in range(0,len(data),2):
     message = json.dumps(message)
     formatted_data.append(message)
 
-final_data = "\n".join(formatted_data)
+train_test_split = 0.8
+split_index = round(len(formatted_data) * train_test_split)
+train = formatted_data[:split_index]
+valid = formatted_data[split_index:]
 
-file = open("train.jsonl","w") 
-file.write(final_data)
+train = "\n".join(train)
+valid = "\n".join(valid)
+
+file = open("train.jsonl", "w") 
+file.write(train)
+
+file = open("valid.jsonl", "w")
+file.write(valid)
