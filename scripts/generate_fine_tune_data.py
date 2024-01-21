f2345301d143c6d174a7ea4bd73a8a7a017d426f
charliehuang09 <charliehuang09@gmail.com>
2024-01-20 16:14:46 -0800

    change train test split percentage from 0.8 to 0.9
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- scripts/generate_fine_tune_data.py
+++ scripts/generate_fine_tune_data.py
@@ -24,7 +24,7 @@ for i in range(0,len(data),2):
     message = json.dumps(message)
     formatted_data.append(message)
 
-train_test_split = 0.8
+train_test_split = 0.9
 split_index = round(len(formatted_data) * train_test_split)
 train = formatted_data[:split_index]
 valid = formatted_data[split_index:]
