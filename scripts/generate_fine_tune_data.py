fb767ca22ec5384a56a0b71919e3765494bb5b8e
charliehuang09 <charliehuang09@gmail.com>
2024-01-28 15:36:04 -0800

    wrap python scripts in main() function
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- scripts/generate_fine_tune_data.py
+++ scripts/generate_fine_tune_data.py
@@ -5,41 +5,47 @@ import json
 
 PATH = '../data/fine_tune'
 
-data = []
-for path in os.listdir(PATH):
-    with open(os.path.join(PATH, path)) as chain:
-        for message in chain:
-            data.append(message)
 
-formatted_data = []
-for i in range(0, len(data), 2):
-    user = data[i]
-    chatbot = data[i + 1]
-    message = {
-        "messages": [{
-            "role": "system",
-            "content": ""
-        }, {
-            "role": "user",
-            "content": user
-        }, {
-            "role": "assistant",
-            "content": chatbot
-        }]
-    }
-    message = json.dumps(message)
-    formatted_data.append(message)
+def main():
+    data = []
+    for path in os.listdir(PATH):
+        with open(os.path.join(PATH, path)) as chain:
+            for message in chain:
+                data.append(message)
 
-train_test_split = 0.9
-split_index = round(len(formatted_data) * train_test_split)
-train = formatted_data[:split_index]
-valid = formatted_data[split_index:]
+    formatted_data = []
+    for i in range(0, len(data), 2):
+        user = data[i]
+        chatbot = data[i + 1]
+        message = {
+            "messages": [{
+                "role": "system",
+                "content": ""
+            }, {
+                "role": "user",
+                "content": user
+            }, {
+                "role": "assistant",
+                "content": chatbot
+            }]
+        }
+        message = json.dumps(message)
+        formatted_data.append(message)
 
-train = "\n".join(train)
-valid = "\n".join(valid)
+    train_test_split = 0.9
+    split_index = round(len(formatted_data) * train_test_split)
+    train = formatted_data[:split_index]
+    valid = formatted_data[split_index:]
 
-file = open("../data/train.jsonl", "w")
-file.write(train)
+    train = "\n".join(train)
+    valid = "\n".join(valid)
 
-file = open("../data/valid.jsonl", "w")
-file.write(valid)
+    file = open("../data/train.jsonl", "w")
+    file.write(train)
+
+    file = open("../data/valid.jsonl", "w")
+    file.write(valid)
+
+
+if __name__ == "__main__":
+    main()
