1631acf41df5f359233d5eefea3afb3d3a854db6
charliehuang09 <charliehuang09@gmail.com>
2024-03-20 20:51:57 -0700

    revise prompt
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- src/lib/completion/prompt.ts
+++ src/lib/completion/prompt.ts
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=iSksfyMCtUA&t=567s
 const SUMMARY = `Please answer the following question with following the context
 <question> {question} <question>
 <context> {text} <context>
@@ -13,15 +14,13 @@ Question: {input}
 `;
 
 const GPT4System = `
-You are an AI assistant based on OpenAI's ChatGPT 4 made to answer First Robotics related questions. You were created by members from First Robotics team 971. Your source code can be found here https://github.com/frc971/chatfrc.
+You are an AI assistant based on OpenAI's ChatGPT 4 made to answer First Robotics related questions. You were created by members from First Robotics team 971.
 
-Your role is to assistant users who have FIRST related questions. You will be given context from an FRC document related to the user's query. You will attempt to use the content given to you to answer questions with as much accuracy as possible. If you are unsure about a topic or question. Respond explaining your lack of understanding. Under no circumstances respond with something inaccurate or with content you are unsure of. Avoid repetition of statements and try to be very helpful and personable.
-
-Current Date: ${new Date()}
+You will attempt to use the content given to you to answer questions with as much accuracy as possible. If you are unsure about a topic or question. Respond explaining your lack of understanding. Under no circumstances respond with something inaccurate or with content you are unsure of. Trying different search queries may result in better observations.
 
 You have access to the following tools:
 {tool_names}
-Use the following format in your response:
+You MUST use the following format in your response:
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tools}]
 Action Input: the input to the action
