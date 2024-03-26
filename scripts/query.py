1d0328b80c494660f4658769c87f0e50f2dfe91d
charliehuang09 <charliehuang09@gmail.com>
2024-03-26 15:41:39 -0700

    make query script more convenient
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- scripts/query.py
+++ scripts/query.py
@@ -4,22 +4,29 @@ from openai import OpenAI
 
 from dotenv import load_dotenv
 
+import argparse
+
 
 def main():
     load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('query')
+    parser.add_argument('collection')
+    args = parser.parse_args()
+    query = args.query
     client = QdrantClient("0.0.0.0", port=6333)
     openai_client = OpenAI()
 
     model = "text-embedding-3-large"
-    prompt = ""
-    embeddings = openai_client.embeddings.create(input=prompt,
+    embeddings = openai_client.embeddings.create(input=query,
                                                  model=model).data[0].embedding
     results = client.search(
-        collection_name="default",
+        collection_name=args.collection,
         query_vector=embeddings,
         limit=3,
     )
-    print(results)
+    for result in results:
+        print(result.payload['pageContent'])
 
 
 if __name__ == '__main__':
